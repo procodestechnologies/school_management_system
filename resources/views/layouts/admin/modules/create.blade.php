79077b7ad45d997080345dfc6c388a7c230e598b
procodestechnologies <procodes41@gmail.com>
2026-07-24 16:21:19 +0300

    dashboard modules controller

---
+++ resources/views/layouts/admin/modules/create.blade.php
@@ -0,0 +1,5 @@
+<x-layouts::app>
+
+
+
+</x-layouts::app>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\ModuleController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
+Route::middleware(['auth', 'verified'])->prefix('dashboard')->group(function () {
+    Route::get('', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::resource('admin/modules', ModuleController::class)->names('modules');
 });
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
